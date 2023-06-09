500fd7577832f242a7886766217c8d1e60e9aa90
Michael Thies <mail@mhthies.de>
2023-06-09 22:20:18 +0200

    Improve codestyle ([..].iter())

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -145,7 +145,7 @@ fn point_to_float_point_distance(p: &Position, x2: f32, y2: f32, game_size: &Pos
         OrderedFloat(((p.x as f32 - x2).powi(2) + ((p.y + game_size.y) as f32 - y2).powi(2)).sqrt()),
         OrderedFloat((((p.x + game_size.x) as f32 - x2).powi(2) + (p.y as f32 - y2).powi(2)).sqrt()),
         OrderedFloat((((p.x + game_size.x) as f32 - x2).powi(2) + ((p.y + game_size.y) as f32 - y2).powi(2)).sqrt()),
-    ].into_iter().min().unwrap().0
+    ].iter().min().unwrap().0
 }
 
 fn point_to_point_distance(p1: &Position, p2: &Position, game_size: &Position) -> f32 {
