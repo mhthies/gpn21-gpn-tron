24f977f6c68e43fce9e1c75ef309c19f45a3ce88
Michael Thies <mail@mhthies.de>
2024-06-01 13:28:20 +0200

    algorithm4: Improve codestyle of direction ranking

--- src/algorithm/algorithm4.rs
+++ src/algorithm/algorithm4.rs
@@ -31,31 +31,40 @@ pub fn decide_action(
     } else {
         if directions.len() == 1 {
             info!("Only one step possible.");
-        } else if directions[0].1.0 != directions[1].1.0 {
+        } else if directions[0].1.has_neighbour_head != directions[1].1.has_neighbour_head {
             info!("Avoiding other head");
-        } else if directions[0].1.1 != directions[1].1.1 {
-            info!("Room score different: {:?}: {}, {:?}: {}", directions[0].0, directions[0].1.1.0, directions[1].0, directions[1].1.1.0);
-        } else if directions[0].1.2 != directions[1].1.2 {
+        } else if directions[0].1.best_empty_space_score_after_step != directions[1].1.best_empty_space_score_after_step {
+            info!("Room score different: {:?}: {}, {:?}: {}", directions[0].0, directions[0].1.best_empty_space_score_after_step.0, directions[1].0, directions[1].1.best_empty_space_score_after_step.0);
+        } else if directions[0].1.direction_score != directions[1].1.direction_score {
             info!("Following wall");
         } else {
-            info!("Better direction: {:?}: {}, {:?}: {}", directions[0].0, directions[0].1.3.0, directions[1].0, directions[1].1.3.0);
+            info!("Better direction: {:?}: {}, {:?}: {}", directions[0].0, directions[0].1.direction_score.0, directions[1].0, directions[1].1.direction_score.0);
         }
     Some(Command::Move(directions[0].0.clone()))
     }
 }
 
-fn rank_direction(d: &MoveDirection, state: &State) -> (bool, OrderedFloat<f32>, bool, OrderedFloat<f32>) {
+
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone)]
+struct DirectionRanking {
+    has_neighbour_head: bool,
+    best_empty_space_score_after_step: OrderedFloat<f32>,
+    has_wall_to_follow: bool,
+    direction_score: OrderedFloat<f32>,
+}
+
+fn rank_direction(d: &MoveDirection, state: &State) -> DirectionRanking {
     let next_position = move_by_direction(&state.my_position, &d, &state.game_size);
     let current_space = explore_empty_space(state, next_position.clone());
-    (
-        has_neighbour_head(&next_position, state),
-        OrderedFloat(calculate_best_empty_space_after_step(
+    DirectionRanking {
+        has_neighbour_head: has_neighbour_head(&next_position, state),
+        best_empty_space_score_after_step: OrderedFloat(calculate_best_empty_space_after_step(
             state,
             &move_by_direction(&state.my_position, &d, &state.game_size),
         )),
-        !(has_wall(&next_position, state) && current_space.next_snake_head_distance > 4),
-        OrderedFloat(evaluate_direction(&d, state)),
-    )
+        has_wall_to_follow: !(has_wall(&next_position, state) && current_space.next_snake_head_distance > 4),
+        direction_score: OrderedFloat(evaluate_direction(&d, state)),
+    }
 }
 
 fn calculate_best_empty_space_after_step(game_state: &State, step_to: &Position) -> f32 {
